ca4a3dc614fdcc77bcbb099ba1dd757f87a64fcc
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-21 20:02:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def join_nested_strings(aoa)
   simplify_it = aoa.flatten
   index_counter = 0
   while index_counter < simplify_it.length
-    if simplify_it[index_counter].instance_of? Integer
+    if simplify_it[index_counter].instance_of? String
       simplify_it.delete_at(index_counter)
     end
     index_counter += 1
