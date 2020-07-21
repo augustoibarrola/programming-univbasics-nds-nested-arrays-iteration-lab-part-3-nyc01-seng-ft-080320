9ebabf566fc5c36c8b2777cfe38d54146d15f93b
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-21 20:00:13 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,19 +1,13 @@
 
-def join_nested_strings(mixed_data)
-  final_array_of_strings_to_be = Array.new
-  row_counter = 0
-  while row_counter < mixed_data.count do
-    index_counter = 0
-    while index_counter < mixed_data[row_counter].count
-      if mixed_data[row_counter][index_counter].is_a? String.class
-        delete_array = mixed_data[row_counter][index_counter]
-        delete_array.delete_at(index_counter)
-        index_counter += 1
-      else
-        index_counter += 1
-      end
+def join_nested_strings(aoa)
+  simplify_it = aoa.flatten
+  index_counter = 0 
+  while index_counter < simplify_it.length
+    if simplify_it[index_counter].instance_of? Integer 
+      simplify_it.delete_at(index_counter)
     end
-    row_counter +=1
+    index_counter += 1
   end
-  final_array_of_strings_to_be.flatten.join(" ")
+  simplify_it.join(' ')
 end
+
